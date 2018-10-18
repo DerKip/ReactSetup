12a88217f08eeea744ba7d00f3f53bcb27ba9ad7
derkip <dkip64@gmail.com>
2018-10-19 00:26:48 +0300

    [Chore] Setup React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component} from "react";
+import "./App.css";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1> Derrick Kirwa</h1>
+        <p>Setting up React from scratch</p>
+      </div>
+    );
+  }
+}
+export default App;
